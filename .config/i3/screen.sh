583898cbe17f173874d7b2c0edb3430c720d8425
Riley Stratton <riley.s.stratton@outlook.com>
2022-09-21 12:50:26 -0600

    made screen.sh executable

--- .config/i3/screen.sh
+++ .config/i3/screen.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ ! -z $(polybar -m | grep eDP-1 ) ]]; then
+if [[ ! -z $(polybar -m | grep "eDP-1") ]]; then
   feh --bg-fill $HOME/.wallpapers/christ-calling-fishermen.3240x2160.jpeg 
 
-elif [[ ! -z $(polybar -m | grep HDMI-0) && ! -z $(polybar -m | grep DP-4) ]]; then
+elif [[ ! -z $(polybar -m | grep "HDMI-0") && ! -z $(polybar -m | grep "DP-4") ]]; then
   # xrandr --output eDP-1 --mode 1920x1200 --rate 60
   feh --bg-fill $HOME/.wallpapers/christ-calling-fishermen.2560x1080.jpeg --bg-fill $HOME/.wallpapers/christ-calling-fishermen.1920x1080.jpeg
 fi
