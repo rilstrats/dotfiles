64ef3cf4b52e3a83ad2ba1bd9db37f36fb7cae7c
Riley Stratton <riley.s.stratton@outlook.com>
2022-09-28 18:44:39 -0600

    moved mega and go to their own files

--- .config/zsh/git.zsh
+++ .config/zsh/git.zsh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 g () {
   if [[ -z $(git status 2> /dev/null) ]]; then
     /bin/git --git-dir=$DOTFILES --work-tree=$HOME $*
@@ -6,39 +8,38 @@ g () {
   fi
 }
 
-gs () {g status}
-gd () {g diff}
-
+gs () {grvo; echo; g status}
+gd () {grvo; g diff}
 gm () {g mv $*}
 
 ga () {g add $*}
 gc () {g commit -m $1}
 gpush () {g push}
 
-gf () {g fetch}
-gpull () {g pull}
+gf () {grvo; echo; g fetch}
+gpull () {grvo; echo; g pull}
 
 gac () {ga .; gc $1}
-gacp () {ga .; gc $1; gp}
+gacp () {ga .; gc $1; gpush}
 
 grv () {g remote -v}
+grvo () {grv | grep origin | head -n 1 | sed "s/^.*\t//g" | sed "s/ (.*$//g"}
+
 grso () {g remote set-url origin $1}
 
 gssh () {
-  repo=$(git remote -v | head -n 1 | sed "s/^.*\.com[/:]//g" | sed "s/ (.*$//g" | sed "s/\.git$//g")
+  repo=$(grvo | sed "s/^.*\.com[/:]//g" | sed "s/\.git$//g")
   read "?Is git@github.com:$repo.git correct? [Y/n]: " input
   correct=`echo ${input:0:1} | tr '[:upper:]' '[:lower:]'`
   echo
 
   if [[ "$correct" == "n" ]]; then
-    echo "Origin not updated, please update with either command: "
-    echo "git remote set-url origin git@github.com:USER/REPO.git"
+    echo "Origin not updated, please update with this command: "
     echo "grso git@github.com:USER/REPO.git"
-    return
+  else
+    grso git@github.com:$repo.git
+    echo "Origin updated to:"
+    git remote -v
   fi
-
-  git remote set-url origin git@github.com:$repo.git
-  echo "Origin updated to:"
-  git remote -v
 }
 
