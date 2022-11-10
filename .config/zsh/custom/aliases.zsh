ee7c80d55d640e1469a00ee9fd7a48bac54ccd5a
Riley Stratton <riley.s.stratton@outlook.com>
2022-11-09 17:01:30 -0700

    moved tmuxh alias and added tmuxer alias

--- .config/zsh/custom/aliases.zsh
+++ .config/zsh/custom/aliases.zsh
@@ -19,3 +19,9 @@ alias kali='docker run --rm -it kalilinux/kali-rolling'
 
 # owd
 alias owd='cd $OLDPWD'
+alias oldpwd='echo $OLDPWD'
+
+# tmux-sessionizer
+alias tmuxer="$HOME/.local/bin/tmux-sessionizer"
+alias tmuxh="tmux attach-session -t home || tmux new-session -s home -c $HOME"
+
