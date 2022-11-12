a5deb5cf39c6ff1c23a035e7cac26277c1410ca7
Riley Stratton <riley.s.stratton@outlook.com>
2022-11-12 08:26:45 -0700

    updated wallpapers to work on laptop

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# WALLPAPER="$HOME/.wallpapers/christ-fishermen"
+WALLPAPER="$HOME/.wallpapers/lost-between"
+# WALLPAPER="$HOME/.wallpapers/tokyo-night"
+
 if [[ ! -z $(xrandr | grep "eDP-1 connected") ]]; then
-  i3lock -i $HOME/.wallpapers/christ-calling-fishermen.3240x2160.png
+  i3lock -i $WALLPAPER.3240x2160.png
 
 elif [[ ! -z $(xrandr | grep "HDMI-0 connected") && ! -z $(xrandr | grep "DP-4 connected") ]]; then
   i3lock-fancy
--- .config/i3/wallpaper.sh
+++ .config/i3/wallpaper.sh
@@ -6,11 +6,11 @@ WALLPAPER="$HOME/.wallpapers/lost-between"
 
 if [[ ! -z $(xrandr | grep "eDP-1 connected") ]]; then
   feh --no-fehb \
-    --bg-center "$WALLPAPER/.3240x2160.jpg"
+    --bg-center $WALLPAPER.3240x2160.jpg
 
 elif [[ ! -z $(xrandr | grep "HDMI-0 connected") && ! -z $(xrandr | grep "DP-4 connected") ]]; then
   feh --no-fehb \
-    --bg-center "$WALLPAPER.2560x1080.jpg" \
-    --bg-center "$WALLPAPER.1920x1080.jpg"
+    --bg-center $WALLPAPER.2560x1080.jpg \
+    --bg-center $WALLPAPER.1920x1080.jpg
 fi
 
