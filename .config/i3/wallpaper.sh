fb128bcdd22dd150ef029772a1c875d40c753be0
Riley Stratton <riley.s.stratton@outlook.com>
2022-11-11 21:43:13 -0700

    updated wallpaper script for new wallpapers

--- .config/i3/wallpaper.sh
+++ .config/i3/wallpaper.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# WALLPAPER="$HOME/.wallpapers/christ-fishermen"
+WALLPAPER="$HOME/.wallpapers/lost-between"
+# WALLPAPER="$HOME/.wallpapers/tokyo-night"
+
 if [[ ! -z $(xrandr | grep "eDP-1 connected") ]]; then
   feh --no-fehb \
-    --bg-fill $HOME/.wallpapers/christ-calling-fishermen.3240x2160.jpeg 
+    --bg-center "$WALLPAPER/.3240x2160.jpg"
 
 elif [[ ! -z $(xrandr | grep "HDMI-0 connected") && ! -z $(xrandr | grep "DP-4 connected") ]]; then
   feh --no-fehb \
-    --bg-fill $HOME/.wallpapers/christ-calling-fishermen.2560x1080.jpeg \
-    --bg-fill $HOME/.wallpapers/christ-calling-fishermen.1920x1080.jpeg
+    --bg-center "$WALLPAPER.2560x1080.jpg" \
+    --bg-center "$WALLPAPER.1920x1080.jpg"
 fi
 
