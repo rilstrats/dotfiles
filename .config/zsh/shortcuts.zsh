11a21e203656d836632d754e055b148fdb288b95
Riley <code@rilstrats.net>
2023-01-09 20:54:11 -0700

    updated semester

--- .config/zsh/shortcuts.zsh
+++ .config/zsh/shortcuts.zsh
@@ -1,25 +1,25 @@
 # code
-export CODEPATH="$HOME/code"
+export CODEPATH=$HOME/code
 alias code="cd $CODEPATH"
-export GOPATH="$CODEPATH"
+export GOPATH=$CODEPATH
 
-export CODESRC="$CODEPATH/src"
+export CODESRC=$CODEPATH/src
 alias codesrc="cd $CODESRC"
 
-export CODEBIN="$CODEPATH/bin"
+export CODEBIN=$CODEPATH/bin
 alias codebin="cd $CODEBIN"
 
-export CODEGH="$CODESRC/github.com"
+export CODEGH=$CODESRC/github.com
 alias codegh="cd $CODEGH"
 
-export CODEGHRS="$CODEGH/rilstrats"
+export CODEGHRS=$CODEGH/rilstrats
 alias codeghrs="cd $CODEGHRS"
 alias ghrs="cd $CODEGHRS"
 
-export CODEGL="$CODESRC/gitlab.com"
+export CODEGL=$CODESRC/gitlab.com
 alias codegl="cd $CODEGL"
 
-export CODEGLRS="$CODEGL/rilstrats"
+export CODEGLRS=$CODEGL/rilstrats
 alias codeglrs="cd $CODEGLRS"
 alias glrs="cd $CODEGLRS"
 
@@ -27,28 +27,28 @@ alias glrs="cd $CODEGLRS"
 # cloud
 if [[ -d $HOME/cloud ]]; then
 
-  export CLOUD="$HOME/cloud"
+  export CLOUD=$HOME/cloud
   alias cloud="cd $CLOUD"
 
-  export CLOUDCOL="$CLOUD/college"
+  export CLOUDCOL=$CLOUD/college
   alias cloudcol="cd $CLOUDCOL"
 
-  export CLOUDSEM="$CLOUDCOL/22-9.fall"
+  export CLOUDSEM=$CLOUDCOL/23-1.winter
   alias cloudsem="cd $CLOUDSEM"
 
-  export CLOUDCYBER="$CLOUD/cyber"
+  export CLOUDCYBER=$CLOUD/cyber
   alias cloudcyber="cd $CLOUDCYBER"
 
-  export CLOUDCOMP="$CLOUDCYBER/competitions"
+  export CLOUDCOMP=$CLOUDCYBER/competitions
   alias cloudcomp="cd $CLOUDCOMP"
 
-  export CLOUDNOTES="$CLOUDCYBER/notes"
+  export CLOUDNOTES=$CLOUDCYBER/notes
   alias cloudnotes="cd $CLOUDNOTES"
 
 fi
 
 # secret (for sensitive configuration files)
-[[ -f "$ZDOTDIR/.secrets.zsh" ]] && source "$ZDOTDIR/.secrets.zsh"
+[[ -f $ZDOTDIR/.secrets.zsh ]] && source $ZDOTDIR/.secrets.zsh
 
 # shortcuts (used by tmux-sessionizer)
 export SHORTCUTS=($CLOUDSEM $CLOUDCOMP $CLOUDNOTES $CODEGHRS $CODEGLRS $CODESEC)
