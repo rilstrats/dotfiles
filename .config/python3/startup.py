404c7ac72d078fb9c125c19bcb806a858c6d1609
Riley <code@rilstrats.net>
2023-02-03 18:12:41 -0700

    fixed python histfile

--- .config/python3/startup.py
+++ .config/python3/startup.py
@@ -3,18 +3,28 @@ import os
 import readline
 
 # histfile = os.path.join(os.getenv("XDG_STATE_HOME"), "python3", "history")
-histfile = os.path.abspath(os.getenv("PYTHONHISTFILE"))
-readline.set_auto_history(False)
+histfile = os.getenv("PYTHONHISTFILE")
+# readline.set_auto_history(False)
 
 try:
     readline.read_history_file(histfile)
-    h_len = readline.get_current_history_length()
-except FileNotFoundError:
-    open(histfile, 'wb').close()
-    h_len = 0
-
-def save(prev_h_len, histfile):
-    new_h_len = readline.get_current_history_length()
+    # default history len is -1 (infinite), which may grow unruly
     readline.set_history_length(1000)
-    readline.append_history_file(new_h_len - prev_h_len, histfile)
-atexit.register(save, h_len, histfile)
+except FileNotFoundError:
+    pass
+
+atexit.register(readline.write_history_file, histfile)
+
+# try:
+#     readline.read_history_file(histfile)
+#     h_len = readline.get_current_history_length()
+# except FileNotFoundError:
+#     open(histfile, 'wb').close()
+#     h_len = 0
+
+# def save(prev_h_len, histfile):
+#     new_h_len = readline.get_current_history_length()
+#     readline.set_history_length(1000)
+#     readline.append_history_file(new_h_len - prev_h_len, histfile)
+
+# atexit.register(save, h_len, histfile)
