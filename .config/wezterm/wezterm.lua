a43240150a34286efa915c91189375e76f64317e
Riley <code@rilstrats.net>
2023-02-24 16:11:54 -0700

    more fonts and wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+
+return {
+  color_scheme = 'tokyonight',
+  font = wezterm.font 'Fira Code Nerd Font Mono',
+  font_size = 12.0,
+  window_background_opacity = 0.9
+}
