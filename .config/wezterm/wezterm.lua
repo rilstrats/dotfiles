07a04f20f39b44845a11b71639857d41b2e9e6e1
Riley <code@rilstrats.net>
2023-02-24 18:08:30 -0700

    wezterm tokyonight

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 
 return {
-  color_scheme = 'tokyonight',
+  color_scheme = 'tokyonight_night',
   font = wezterm.font 'Fira Code Nerd Font Mono',
   font_size = 12.0,
   window_background_opacity = 0.9
