aac021c2176e20ecb04c396c3153bd4ffa6937e6
Riley <code@rilstrats.net>
2023-02-28 14:26:19 -0700

    reorganized git.zsh

--- .config/zsh/git.zsh
+++ .config/zsh/git.zsh
@@ -1,52 +1,54 @@
 # dotfiles
-export DOTFILES=$XDG_CONFIG_HOME/dotfiles.git
-export DFS_OPTS="--git-dir=$DOTFILES --work-tree=$HOME"
-alias dfs=dotfiles
-dotfiles() {/usr/bin/git $(echo $DFS_OPTS) $*}
+# export DOTFILES=$XDG_CONFIG_HOME/dotfiles.git
+# export DFS_OPTS="--git-dir=$DOTFILES --work-tree=$HOME"
+# alias dfs=dotfiles
+# dotfiles() {/usr/bin/git $(echo $DFS_OPTS) $*}
 
 # git
 alias g=git
 git() {
   if [[ $PWD == $HOME || $PWD =~ $XDG_CONFIG_HOME.* ]]; then
-    OPTS=$DFS_OPTS
+    OPTS="--git-dir=$DOTFILES --work-tree=$HOME"
   fi
   /usr/bin/git $(echo $OPTS) $*
   unset OPTS
 }
 
-gs() {g status $*}
-gd() {g diff $*}
-gm() {g mv $*}
-gr() {g restore $*}
-gp() {echo "Use gpull/gpl or gpush/gps"}
+gs() {git status $*}
+gd() {git diff $*}
+gm() {git mv $*}
+gr() {git restore $*}
 
-gf() {g fetch $*}
+gf() {git fetch $*}
 gpull() {g pull $*}
-alias gpl=gpull
 
-ga() {g add $*}
-gc() {g commit -m $*}
-gpush () {g push $*}
-alias gps=gpush
+ga() {git add $*}
+gc() {git commit $*}
+gcm() {git commit -m $*}
+gpush() {git push $*}
 
-gac() {ga .; gc "$*"}
-gacp() {ga .; gc "$*"; gpush}
+gac() {ga .; gc}
+gacm() {ga .; gcm $*}
+gacp() {ga .; gc; gpush}
+gcp() {gc; gpush}
 
-grv() {g remote -v $*}
-gro() {grv | grep origin | head -n 1 | sed "s/^.*\t//g" | sed "s/ (.*$//g"}
+grv() {g remote -v}
+grvo() {
+  grv | grep origin | head -n 1 | sed 'ls/^.*\t//g' | sed 's/ (.*$//g'
+}
 
 grso () {g remote set-url origin $*}
 gssh () {
-  repo=$(gro | sed "s/^.*\.com[/:]//g" | sed "s/\.git$//g")
+  repo=$(grvo | sed 's/^.*\.com[/:]//g' | sed 's/\.git$//g')
   read "input?Is git@github.com:$repo.git correct? [y/N]: "
-  update=`echo ${input:0:1} | tr '[:upper:]' '[:lower:]'`
+  update=$(echo ${input:0:1} | tr '[:upper:]' '[:lower:]')
 
   if [[ ! "$update" == "y" ]]; then
     echo "Origin not updated. Update using: "
     echo "grso git@github.com:USER/REPO.git"
   else
     grso git@github.com:$repo.git
-    echo "Origin updated to $(gro)"
+    echo "Origin updated to $(grvo)"
   fi
 }
 
