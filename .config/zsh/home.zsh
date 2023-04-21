416260cb22f827e18bbf8ec4d88ca74e715c4ad9
Riley <code@rilstrats.net>
2023-04-20 22:01:59 -0600

    removed viminit variables

--- .config/zsh/home.zsh
+++ .config/zsh/home.zsh
@@ -2,5 +2,5 @@ alias yarn="yarn --use-yarnrc $XDG_CONFIG_HOME/yarn/config"
 alias svn="svn --config-dir $XDG_CONFIG_HOME/subversion"
 # alias wget="wget --hsts-file=$XDG_CACHE_HOME/wget-hsts"
 
-export GVIMINIT='let $MYGVIMRC = !has("nvim") ? "$XDG_CONFIG_HOME/vim/gvimrc" : "$XDG_CONFIG_HOME/nvim/init.gvim" | source $MYGVIMRC'
-export VIMINIT='let $MYVIMRC = !has("nvim") ? "$XDG_CONFIG_HOME/vim/vimrc" : "$XDG_CONFIG_HOME/nvim/init.lua" | source $MYVIMRC'
+# export GVIMINIT='let $MYGVIMRC = !has("nvim") ? "$XDG_CONFIG_HOME/vim/gvimrc" : "$XDG_CONFIG_HOME/nvim/init.gvim" | source $MYGVIMRC'
+# export VIMINIT='let $MYVIMRC = !has("nvim") ? "$XDG_CONFIG_HOME/vim/vimrc" : "$XDG_CONFIG_HOME/nvim/init.lua" | source $MYVIMRC'
