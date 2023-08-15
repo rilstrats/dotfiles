6ac4b36093e15c6c78f413d8ab15e2980f62b0ae
Riley <91571914+rilstrats@users.noreply.github.com>
2023-08-15 15:12:22 -0600

    updated zellij-sessionizer to include classes

--- .config/zsh/shortcuts.zsh
+++ .config/zsh/shortcuts.zsh
@@ -1,55 +1,21 @@
 # code
-export CODEPATH=$HOME/code
-alias code="cd $CODEPATH"
-
-export CODESRC=$CODEPATH
-alias codesrc="cd $CODESRC"
-
-export CODEGH=$CODESRC/github.com
-alias codegh="cd $CODEGH"
-
-export CODEGHRS=$CODEGH/rilstrats
-alias codeghrs="cd $CODEGHRS"
-alias ghrs="cd $CODEGHRS"
-
-export CODEGL=$CODESRC/gitlab.com
-alias codegl="cd $CODEGL"
-
-export CODEGLRS=$CODEGL/rilstrats
-alias codeglrs="cd $CODEGLRS"
-alias glrs="cd $CODEGLRS"
-
+if [[ -d $HOME/code ]]; then
+  export CODEPATH=$HOME/code
+  alias codepath="cd $CODEPATH"
+fi
 
 # cloud
 if [[ -d $HOME/cloud ]]; then
-
   export CLOUDPATH=$HOME/cloud
-  alias cloud="cd $CLOUDPATH"
+  alias cloudpath="cd $CLOUDPATH"
 
-  export CLOUDCOL=$CLOUDPATH/college
-  alias cloudcol="cd $CLOUDCOL"
+  export CLOUDEDU=$CLOUDPATH/education
+  alias cloudedu="cd $CLOUDEDU"
 
-  export CLOUDSEM=$CLOUDCOL/2023-04_spring
-  alias cloudsem="cd $CLOUDSEM"
-
-  export CLOUDTECH=$CLOUDPATH/tech
-  alias cloudtech="cd $CLOUDTECH"
-
-  export CLOUDCYBER=$CLOUDTECH/cyber
+  export CLOUDCYBER=$CLOUDPATH/tech/cyber
   alias cloudcyber="cd $CLOUDCYBER"
-
-  export CLOUDCOMP=$CLOUDCYBER/competitions
-  alias cloudcomp="cd $CLOUDCOMP"
-
-  export CLOUDNOTES=$CLOUDCYBER/notes
-  alias cloudnotes="cd $CLOUDNOTES"
-
 fi
 
-# secret (for sensitive configuration files)
-# [[ -f $ZDOTDIR/.secrets.zsh ]] && source $ZDOTDIR/.secrets.zsh
-
 # shortcuts (used by tmux-sessionizer)
-# export CODEREPOS=$(repofind $CODESRC | tr '\n' ' ')
-export SHORTCUTS=($CLOUDSEM $CLOUDCOMP)
+export SHORTCUTS=($CLOUDCYBER)
 
