5a932f68acb82a3a9ce1864e0d1b972986a0d933
Riley <91571914+rilstrats@users.noreply.github.com>
2023-11-18 19:36:45 -0700

    upgraded font size to 14

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require 'wezterm'
 return {
   color_scheme = 'tokyonight_night',
   font = wezterm.font 'Fira Code Nerd Font Mono',
-  font_size = 12.0,
+  font_size = 14.0,
   window_background_opacity = 0.9,
   window_close_confirmation = 'NeverPrompt',
 }
