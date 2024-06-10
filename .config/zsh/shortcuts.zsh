14c87596e988456e3d7bb896a0b77c5a4942a1a1
Riley <91571914+rilstrats@users.noreply.github.com>
2024-06-10 17:25:59 -0600

    updated zellij-sessionizer to work with a proper array

--- .config/zsh/shortcuts.zsh
+++ .config/zsh/shortcuts.zsh
@@ -1,16 +1,16 @@
-ZELLIJ_SHORTCUTS=()
+ZELLIJ_SHORTCUTS=($HOME/ $HOME/.dotfiles/)
 
 # code
 if [[ -d $HOME/code ]]; then
   export CODEPATH=$HOME/code # necessary cause used by git-codeclone
-  ZELLIJ_SHORTCUTS+=("$(fd -HI '.git$' $CODEPATH | sed 's|/\.git||')")
+  ZELLIJ_SHORTCUTS+=($(fd -HI '.git$' $CODEPATH | sed 's|\.git/||'))
 fi
 
 # cloud
 if [[ -d $HOME/cloud ]]; then
-  ZELLIJ_SHORTCUTS+=("$HOME/cloud/notes")
-  ZELLIJ_SHORTCUTS+=("$(fd -HI . $HOME/cloud/education --exact-depth 3 --type d)")
-  ZELLIJ_SHORTCUTS+=("$(fd -HI . $HOME/cloud/tech/cyber --exact-depth 1 --type d)")
+  ZELLIJ_SHORTCUTS+=($HOME/cloud/notes/)
+  ZELLIJ_SHORTCUTS+=($(fd -HI . $HOME/cloud/education --exact-depth 3 --type d))
+  ZELLIJ_SHORTCUTS+=($(fd -HI . $HOME/cloud/tech/cyber --exact-depth 1 --type d))
 fi
 
 export ZELLIJ_SHORTCUTS
