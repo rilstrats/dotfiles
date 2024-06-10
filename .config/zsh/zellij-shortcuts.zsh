51dbea6dfd7a20f0ec604afd65515b19d5ac8d8b
Riley <91571914+rilstrats@users.noreply.github.com>
2024-06-10 13:43:54 -0600

    fixed zellij-shortcuts.zsh

--- .config/zsh/zellij-shortcuts.zsh
+++ .config/zsh/zellij-shortcuts.zsh
@@ -2,18 +2,14 @@ ZELLIJ_SHORTCUTS=()
 
 # code
 if [[ -d $HOME/code ]]; then
-  ZELLIJ_SHORTCUTS+=($(fd -HI '.git$' $HOME/code | sed 's|/\.git||'))
+  ZELLIJ_SHORTCUTS+=("$(fd -HI '.git$' $HOME/code | sed 's|/\.git||')")
 fi
 
 # cloud
 if [[ -d $HOME/cloud ]]; then
-  ZELLIJ_SHORTCUTS+=($HOME/cloud/notes)
-
-  CLOUDEDU=$CLOUDPATH/education
-  ZELLIJ_SHORTCUTS+=($(fd -HI . $CLOUDEDU --exact-depth 3 --type d))
-
-  CLOUDCYBER=$CLOUDPATH/tech/cyber
-  ZELLIJ_SHORTCUTS+=($(fd -HI . $CLOUDCYBER --exact-depth 1 --type d))
+  ZELLIJ_SHORTCUTS+=("$HOME/cloud/notes")
+  ZELLIJ_SHORTCUTS+=("$(fd -HI . $HOME/cloud/education --exact-depth 3 --type d)")
+  ZELLIJ_SHORTCUTS+=("$(fd -HI . $HOME/cloud/tech/cyber --exact-depth 1 --type d)")
 fi
 
 export ZELLIJ_SHORTCUTS
