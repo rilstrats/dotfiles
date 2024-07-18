1d01315f280e43f2c793d165ff888c730a8c23cf
Riley <91571914+rilstrats@users.noreply.github.com>
2024-07-18 09:34:20 -0600

    checked if directories exist before adding them

--- .config/zsh/shortcuts.zsh
+++ .config/zsh/shortcuts.zsh
@@ -8,9 +8,9 @@ fi
 
 # cloud
 if [[ -d $HOME/cloud ]]; then
-  ZELLIJ_SHORTCUTS+=($HOME/cloud/notes/)
-  ZELLIJ_SHORTCUTS+=($(fd -HI . $HOME/cloud/education --exact-depth 3 --type d))
-  ZELLIJ_SHORTCUTS+=($(fd -HI . $HOME/cloud/tech/cyber --exact-depth 1 --type d))
+  [[ -d $HOME/cloud/notes ]] && ZELLIJ_SHORTCUTS+=($HOME/cloud/notes/)
+  [[ -d $HOME/cloud/education ]] && ZELLIJ_SHORTCUTS+=($(fd -HI . $HOME/cloud/education --exact-depth 3 --type d))
+  [[ -d $HOME/cloud/tech/cyber ]] && ZELLIJ_SHORTCUTS+=($(fd -HI . $HOME/cloud/tech/cyber --exact-depth 1 --type d))
 fi
 
 export ZELLIJ_SHORTCUTS
