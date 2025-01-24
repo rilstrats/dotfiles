160ad1fa277517cb6d6337254ea5bba56e561ba8
Riley <91571914+rilstrats@users.noreply.github.com>
2025-01-23 21:51:59 -0700

    followed advent of neovim day 3

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,13 @@
 print("advent of neovim")
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+vim.api.nvim_create_autocmd('TextYankPost',  {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
