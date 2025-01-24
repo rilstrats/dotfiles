09868a1d87f3a2b0eef12fcd84e0f4a291c28b0b
Riley <91571914+rilstrats@users.noreply.github.com>
2025-01-23 22:33:15 -0700

    added transparency and tokyonight colorscheme

---
+++ .config/nvim/lua/plugins/colors.lua
@@ -0,0 +1,4 @@
+return {
+  { "folke/tokyonight.nvim", config = function() vim.cmd.colorscheme "tokyonight" end },
+  { "xiyaowong/transparent.nvim", config = function() vim.cmd "TransparentEnable" end }
+}
