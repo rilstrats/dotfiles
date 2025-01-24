f2e2c8a0411da697226eb8676b9d87e6e7fcbd00
Riley <91571914+rilstrats@users.noreply.github.com>
2025-01-23 22:32:38 -0700

    followed advent of neovim day 4

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,6 +4,8 @@ vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 
+require("config.lazy")
+
 vim.api.nvim_create_autocmd('TextYankPost',  {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
