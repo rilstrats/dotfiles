7740d2e73b10c0197bb363232712221eca93ce78
Riley <91571914+rilstrats@users.noreply.github.com>
2025-07-10 13:46:14 -0600

    check for nonstandard tools before executing

--- .config/zsh/shortcuts.zsh
+++ .config/zsh/shortcuts.zsh
@@ -3,15 +3,25 @@ ZELLIJ_SHORTCUTS=($HOME/ $HOME/.dotfiles/ $HOME/.dotfiles/.config/nvim)
 # code
 if [[ -d $HOME/code ]]; then
   export CODEPATH=$HOME/code # necessary cause used by git-codeclone
-  ZELLIJ_SHORTCUTS+=($(fd -HI '.git$' $CODEPATH | sed 's|\.git/||'))
+  if command -v fd &> /dev/null; then
+    ZELLIJ_SHORTCUTS+=($(fd -HI '.git$' $CODEPATH | sed 's|\.git/||'))
+  else
+    ZELLIJ_SHORTCUTS+=($(find $CODEPATH -name ".git" | sed 's|\.git||'))
+  fi
 fi
 
 # cloud
 if [[ -d $HOME/cloud ]]; then
   EDU=$HOME/cloud/edu
-  [[ -d $EDU ]] && ZELLIJ_SHORTCUTS+=($(fd -HI . $EDU --exact-depth 3 -t d))
   CYB=$HOME/cloud/tech/cyber
-  [[ -d $CYB ]] && ZELLIJ_SHORTCUTS+=($(fd -HI . $CYB --exact-depth 1 -t d))
+
+  if command -v fd &> /dev/null; then
+    [[ -d $EDU ]] && ZELLIJ_SHORTCUTS+=($(fd -u . $EDU --exact-depth 3 -t d))
+    [[ -d $CYB ]] && ZELLIJ_SHORTCUTS+=($(fd -u . $CYB --exact-depth 1 -t d))
+  else
+    [[ -d $EDU ]] && ZELLIJ_SHORTCUTS+=($(find $EDU -mindepth 3 -maxdepth 3 -type d))
+    [[ -d $CYB ]] && ZELLIJ_SHORTCUTS+=($(find $CYB -mindepth 3 -maxdepth 3 -type d))
+  fi
 fi
 
 export ZELLIJ_SHORTCUTS
