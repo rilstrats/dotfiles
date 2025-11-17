145eb7b1ab7d1aff04fa85c1c376248179009314
Riley <91571914+rilstrats@users.noreply.github.com>
2025-11-17 16:52:35 -0700

    nvim: completed advent of nvim day 9
    added telescope

---
+++ .config/nvim-advent/lua/plugins/telescope.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = 'v0.1.9',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install' }
+    },
+    config = function()
+      require('telescope').setup { extensions = { fzf = {} } }
+      require('telescope').load_extension('fzf')
+      vim.keymap.set("n", "<leader>sf", require('telescope.builtin').find_files)
+      vim.keymap.set("n", "<leader>sg", require('telescope.builtin').live_grep)
+      vim.keymap.set("n", "<leader>sb", require('telescope.builtin').buffers)
+      vim.keymap.set("n", "<leader>sh", require('telescope.builtin').help_tags)
+      vim.keymap.set("n", "<leader>sn", function()
+        require('telescope.builtin').find_files {
+          cwd = vim.fn.stdpath("config")
+        }
+      end)
+    end
+  },
+}
